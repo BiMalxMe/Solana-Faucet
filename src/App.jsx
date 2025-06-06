32dcbe5693dac4d7f182f2226547c5457da29d6c
BiMalxMe <journeyxbimal@gmail.com>
2025-06-06 08:09:30 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import '@solana/wallet-adapter-react-ui/styles.css';
+import "./App.css";
+import {
+  WalletDisconnectButton,
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { RequestAirdrop } from "./Components/Airdrop";
+import { Bar } from "./Components/Bar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     Hello kta hoo
-    </>
-  )
+    // Ensure these providers wrap your entire application where wallet functionality is needed
+    <ConnectionProvider endpoint={"https://solana-devnet.g.alchemy.com/v2/KwSxcRvEz-gO60Q7FPyQwtiyA0OLb6Mi"}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          <WalletDisconnectButton />
+          <RequestAirdrop />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Bar.jsx
@@ -0,0 +1,7 @@
+export const Bar = () => {
+    return (
+      <div className="sticky top-4 bg-[#28282B] text-blue-500 w-2/3 flex justify-center items-center rounded-lg p-4">
+        SOL Faucet
+      </div>
+    );
+  };
